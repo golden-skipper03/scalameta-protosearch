b1a1b8686f97e9c19983f44c2cd6f567ae2c77c9
golden-skipper03 <157791077+golden-skipper03@users.noreply.github.com>
2024-07-18 19:45:03 +0530

    scaladocinfo

--- scalameta-demo/src/main/scala/ScalaParser.scala
+++ scalameta-demo/src/main/scala/ScalaParser.scala
@@ -3,34 +3,51 @@ import scala.meta.contrib._
 import scala.meta.contrib.DocToken._
 import scala.meta.tokens.Token.Comment
 
+case class ScaladocInfo(description: String, params: List[String], tparams: List[String])
+
 object ScalaParser {
 
-  def parseAndExtractInfo(source: String): (List[String],  List[String], List[String], List[String]) = {
+  def parseAndExtractInfo(source: String): Map[String, ScaladocInfo] = {
     val parsed: Source = source.parse[Source].get
 
     val comments = parsed.tokens.collect {
       case comment: Comment if comment.syntax.startsWith("/**") =>
-        ScaladocParser.parseScaladoc(comment).getOrElse(Nil)
-    }.flatten.toList
-
-    val descriptions = comments.collect {
-      case DocToken(Description, content, _) => content
-    }.flatten.toList
-
-    val params = comments.collect {
-      case DocToken(Param, name, desc) => s"@$name: $desc"
-    }.toList
-
+        (comment.pos.startLine, ScaladocParser.parseScaladoc(comment).getOrElse(Nil))
+    }
 
     val functions = parsed.collect {
-      case Defn.Def(_, name, _, _, _, _) => name.value
-    }.toList
+      case defn @ Defn.Def(_, name, _, _, _, _) => (name.value, defn.pos.startLine)
+    }
 
-    val classes = parsed.collect {
-      case Defn.Class(_, name, _, _, _) => name.value
-    }.toList
+    var scaladocInfoMap = Map[String, ScaladocInfo]()
 
-    (descriptions, params,  functions, classes)
+    var lastComment = -1
+    functions.foreach { case (functionName, functionLine) =>
+      while (lastComment + 1 < comments.length && comments(lastComment + 1)._1 < functionLine) {
+        lastComment += 1
+      }
+      if (lastComment >= 0) {
+        val tokens = comments(lastComment)._2
+        
+        val descriptions = tokens.collect {
+          case DocToken(Description, content, _) => content.getOrElse("")
+        }.mkString(" ")
+
+        val params = tokens.collect {
+          case DocToken(Param, Some(name), desc) => s"@$name: ${desc.getOrElse("")}"
+        }
+
+        val tparams = tokens.collect {
+          case DocToken(TypeParam, Some(name), desc) => s"@tparam $name: ${desc.getOrElse("")}"
+        }
+
+        scaladocInfoMap += (functionName -> ScaladocInfo(descriptions, params, tparams))
+      } else {
+        scaladocInfoMap += (functionName -> ScaladocInfo("", Nil, Nil))
+      }
+    }
+
+    scaladocInfoMap
   }
 }
 
@@ -39,20 +56,37 @@ object HelloWorld extends App {
 
   val source = """
     /** This is a Scaladoc comment
-      * @param a The first parameter
-      * @param b The second parameter
+      * This is a description for the object Main.
       */
-
-      class sample{}
     object Main {
+      /** 
+        * This function sums two integers.
+        * @param a The first parameter
+        * @param b The second parameter
+        */
       def sum[T](a: Int, b: Int): Int = a + b
+
+      /** 
+        * This function greets the user.
+        * @param name The name parameter
+        */
+      def greet(name: String): Unit = println(s"Hello, $name!")
+
+      /** 
+        * This function subtracts two integers.
+        * @param c The first parameter to subtract
+        * @param d The second parameter to subtract
+        */
+      def subtract[T](c: Int, d: Int): Int = c - d
     }
   """
 
-  val (descriptions, params,  functions, classes) = ScalaParser.parseAndExtractInfo(source)
+  val scaladocInfoMap = ScalaParser.parseAndExtractInfo(source)
 
-  println(s"Descriptions: ${descriptions.mkString("\n")}")
-  println(s"Params: ${params.mkString("\n")}")
-  println(s"Functions: ${functions.mkString(", ")}")
-  println(s"Classes: ${classes.mkString(", ")}")
+  scaladocInfoMap.foreach { case (functionName, info) =>
+    println(s"Function: $functionName")
+    println(s"  Description: ${info.description}")
+    println(s"  Params: ${info.params.mkString(", ")}")
+    println(s"  Type Params: ${info.tparams.mkString(", ")}")
+  }
 }
